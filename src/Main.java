86799cf03c79c06798ff1edb12ff05061ab3bca0
olgai <olgaiakovleva2308@gmail.com>
2023-01-29 23:44:43 +0300

    Задачи к домашке Переменные часть 1

---
+++ src/Main.java
@@ -0,0 +1,61 @@
+public class Main {
+    public static void main(String[] args) {
+        var dog = 8.0;
+        System.out.println(dog);
+        var cat = 3.6;
+        System.out.println(cat);
+        var paper = 763789;
+        System.out.println(paper);
+        dog = dog + 4;
+        System.out.println(dog);
+        cat = cat + 4;
+        System.out.println(cat);
+        paper = paper + 4;
+        System.out.println(paper);
+        dog = dog- 3.5;
+        System.out.println(dog);
+        cat = cat - 1.6;
+        System.out.println(cat);
+        paper = paper - 7639;
+        System.out.println(paper);
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+        var firstBoxerWeight = 78.2;
+        var secondBoxerWeight = 82.7;
+        var bothBoxersWeight = firstBoxerWeight + secondBoxerWeight;
+        System.out.println("Общий вес двух боксеров " + bothBoxersWeight + "кг");
+        var weightDifference = secondBoxerWeight - firstBoxerWeight;
+        System.out.println("Разница в весе двух боксеров " + weightDifference + "кг");
+var weightDifferenceType2 = ( secondBoxerWeight % firstBoxerWeight);
+System.out.println("Разница в весе " + weightDifferenceType2 + "кг" );
+var totalWorkHours = 640;
+var hoursPerDayForEachPerson = 8;
+var numberOfWorkers = totalWorkHours / hoursPerDayForEachPerson;
+System.out.println("Всего работников в компании - " + numberOfWorkers + " человек");
+var extendedNumberOfWorkers = numberOfWorkers + 94;
+var extraWorkHours = 94 * hoursPerDayForEachPerson + totalWorkHours;
+System.out.println("Если в компании работает " + extendedNumberOfWorkers + " человек, то всего " + extraWorkHours + " часов работы может быть поделено между сотрудниками");
+
+
+
+
+
+
+
+
+    }}
+
+
+
